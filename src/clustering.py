ceb66af9cfba075fc176c2f847b659493fd19394
Daniel Gil <danielgil@DanielGil-MacBook-Pro.local>
2019-09-29 08:23:28 +1000

    Some tests sampling 10% of data

--- src/clustering.py
+++ src/clustering.py
@@ -1,12 +1,16 @@
 import utils
 import pandas as pd
 
+
 import sklearn
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.cluster import OPTICS, cluster_optics_dbscan
+from sklearn.ensemble import IsolationForest
+
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import silhouette_score
 
 def kmeans_train(X,n_clusters=1):
     """Perform Kmeans clustering
@@ -36,6 +40,32 @@ def kmeans_predict(X,model):
     """
     labels=model.predict(X)
     return labels
+
+def kmeans_get_number_clusters(X,max_clusters=7):
+    """Get the score for a range number of clusters for kmeans using silhouette technique.
+    
+    Parameters
+    ----------
+    X : Matrix, shape = [n_samples, n_features]
+        
+    Returns
+    -------
+    df_silhouette : dataframe, shape [n_cluster,2]
+        Index of the cluster each sample belongs to.
+    """
+    scores = []
+    clusters = range(2,max_clusters)
+    for K in clusters:
+        
+        clusterer = KMeans(n_clusters=K)
+        cluster_labels = clusterer.fit_predict(X)
+        score = silhouette_score(X, cluster_labels)
+        scores.append(score)
+        
+    # Plot it out
+    df_silhouette = pd.DataFrame({'Num Clusters':clusters, 'score':scores})
+    return df_silhouette
+    
 
 def clustering_print_results(original_df,labels, features, X=None,print_out=True, plot_out=False):
     """Prints and plots clustering results.
@@ -71,7 +101,7 @@ def clustering_print_results(original_df,labels, features, X=None,print_out=True
         fig, ax = plt.subplots()
         for key, cluster in clusters:
             cluster.plot(ax=ax, kind='scatter', x='x', y='y', alpha=0.5, s=250,label='Cluster: {:d}'.format(key), color=colors[key])
-            fig.savefig("../outputs/clustering/"+str(utils.get_timestamp())+".png")
+        fig.savefig("../outputs/clustering/"+str(utils.get_timestamp())+".png")
 
     if (print_out):
         for key, cluster in clusters:
@@ -85,7 +115,9 @@ def dbscan_fit_predict(eps,min_samples,X):
     """Perform DBSCAN clustering from features or distance matrix.
     Parameters
     ----------
-    X : matrix of shape (n_samples, n_features)
+    X           : matrix of shape (n_samples, n_features)
+    eps         : The maximum distance between two samples for one to be considered as in the neighborhood of the other.
+    min_samples : The number of samples (or total weight) in a neighborhood for a point to be considered as a core point. This includes the point itself
     
     Returns
     ----------
@@ -106,7 +138,7 @@ def dbscan_fit_predict(eps,min_samples,X):
 
 #optics_fit_predict(min_samples=50, xi=0.05, min_cluster_size=0.05, eps=2, X):
 
-def optics_fit_predict(min_samples, xi, min_cluster_size, eps, X):
+def optics_fit_predict(X,min_samples=50, cluster_method='dbscan', eps=2):
     """Perform OPTICS clustering
     Extracts an ordered list of points and reachability distances, and
     performs initial clustering using ``max_eps`` distance specified at
@@ -114,15 +146,30 @@ def optics_fit_predict(min_samples, xi, min_cluster_size, eps, X):
     
     Parameters
     ----------
-    X : array, shape (n_samples, n_features), or (n_samples, n_samples)  
+    X               : array, shape (n_samples, n_features), or (n_samples, n_samples)  
+    min_samples     : The number of samples in a neighborhood for a point to be considered as a core point.
+    cluster_method  : 'dbscan' by default. Other available: 'xi'
+    eps             : The maximum distance between two samples for one to be considered as in the neighborhood of the other.
 
     Returns
     -------
     labels: Prediction/labels  
     """
-    opt= OPTICS(min_samples=min_samples, xi=xi, min_cluster_size=min_cluster_size)
+    opt= OPTICS(min_samples=min_samples, cluster_method=str(cluster_method))
     opt.fit(X)
     labels=cluster_optics_dbscan(reachability=opt.reachability_,
                                    core_distances=opt.core_distances_,
                                    ordering=opt.ordering_, eps=eps)
+
+    return labels
+
+
+def iforest_train(X,contam=0.25):
+    
+    iforest = IsolationForest(behaviour='new', contamination=contam)
+    iforest.fit(X)
+    return iforest
+
+def iforest_predict(X,model):
+    labels=model.predict(X)
     return labels
